baa84b58f2b3591e01ac92cfb3cbf3d1e6d88452
banseok1216 <bansuk1216@naver.com>
2025-06-13 17:56:34 +0900

    feat(navigation): 탭 레이아웃에 홈/프로필 탭 설정 - TabLayout에 index, profile 탭 화면 추가 - '쉐프토리' / '홈' 타이틀 및 탭 라벨 설정 - '프로필' / '마이' 탭 라벨 및 헤더 설정
    feat(main-ui): 메인 홈 화면 기능 및 레이아웃 구현
    - HomeScreen 구성
      - 레시피 목록 조회 및 렌더링 (`useRecipeListViewModel`)
      - 추천/최근 본 레시피 섹션 분리
      - RecipeList 아이템 클릭 시 상세 페이지 이동
    - 화면 상단에 요약 설명 및 링크 입력 UI 배치
    
    feat(main-link-input-component): 링크 입력용 LinkInput 컴포넌트 추가
    - 커스텀 TextInput 컴포넌트 생성
    
    feat(shared-ui): 로딩 시 스켈레톤 표시용 LoadingView 컴포넌트 추가
    - react-native-reanimated-skeleton 기반 스켈레톤 UI 추가
    - loading 상태에 따라 children 대신 placeholder 보여줌
    
    feat(main-ui): 섹션 구분용 HomeSectionHeader 컴포넌트 추가
    - 제목과 '전체 보기' 버튼이 있는 섹션 헤더 컴포넌트 구현
    - 'HomeScreen' 에서 재사용성 확보

---
+++ src/app/(tabs)/_layout.tsx
@@ -0,0 +1,16 @@
+import { Tabs } from 'expo-router';
+
+export default function TabLayout() {
+    return (
+        <Tabs>
+            <Tabs.Screen name="index" options={{
+                headerTitle: '쉐프토리',
+                tabBarLabel: '홈'
+            }} />
+            <Tabs.Screen name="profile" options={{
+                headerTitle: '프로필' ,
+                tabBarLabel: '마이'
+            }} />
+        </Tabs>
+    );
+}
