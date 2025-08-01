e237906123de7f73e82b66d4fefb1b96985ef77b
황교준 <hwangkyojun7806@gmail.com>
2025-08-01 14:16:52 +0900

    feat: 마이페이지 구현

--- src/modules/login/api/api.ts
+++ src/modules/login/api/api.ts
@@ -1,7 +1,7 @@
 import { client } from "@/src/modules/shared/api/client";
 
 export const getUser : ()=>Promise<User | null> = async () => {
-  const response = await client.get("/user/me");
+  const response = await client.get("/users/me");
   if(!response.data){
     throw new Error("응답에 body가 없습니다.");
   }
