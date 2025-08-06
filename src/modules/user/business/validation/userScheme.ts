4c0af7735bd563f2fa1ba8eca010c22800b15211
황교준 <hwangkyojun7806@gmail.com>
2025-08-06 18:09:40 +0900

    fix : 빈 문자열만 아니면 닉네임 입력할 수 있게 변경

--- src/modules/user/business/validation/userScheme.ts
+++ src/modules/user/business/validation/userScheme.ts
@@ -4,11 +4,9 @@ import { Gender } from "../../enums/Gender";
 
 export const userSchema = z.object({
   gender: z.enum(Object.keys(Gender)).transform((val) => val as Gender),
-  nickname: z
-    .string()
-    .min(2)
-    .max(10)
-    .regex(/^[가-힣a-zA-Z0-9\s]*$/, "한글, 영문, 숫자만 입력 가능합니다"),
+  nickname: z.string()
+  .trim()
+  .min(1, "닉네임을 입력해주세요"),
   dateOfBirth: z.custom<DateOnly>(
     (val) => val instanceof DateOnly,
     "유효한 DateOnly 객체를 입력해주세요",
