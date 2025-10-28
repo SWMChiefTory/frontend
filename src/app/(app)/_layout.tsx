45b6b4260bae39c833dbad614e0fb20165e79788
황교준 <hwangkyojun7806@gmail.com>
2025-10-28 22:00:29 +0900

    feat : 웹뷰에서 safe area 조절할 수 있게 변경

--- src/app/(app)/_layout.tsx
+++ src/app/(app)/_layout.tsx
@@ -1,39 +1,20 @@
 import { Stack, useLocalSearchParams } from "expo-router";
-import { Platform } from "react-native";
 import { KeyboardProvider } from "react-native-keyboard-controller";
-import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function AppLayout() {
   const { setIsWebviewLoaded } = useLocalSearchParams();
 
   return (
     <KeyboardProvider statusBarTranslucent navigationBarTranslucent>
-      <SafeAreaSpecificToPlatform>
-        <Stack>
-          <Stack.Screen
-            name="index"
-            options={{ headerShown: false }}
-            initialParams={{ setIsWebviewLoaded }}
-          />
-          <Stack.Screen name="settings/settings" />
-          <Stack.Screen name="recipe/create" />
-        </Stack>
-        </SafeAreaSpecificToPlatform>
+      <Stack>
+        <Stack.Screen
+          name="index"
+          options={{ headerShown: false }}
+          initialParams={{ setIsWebviewLoaded }}
+        />
+        <Stack.Screen name="settings/settings" />
+        <Stack.Screen name="recipe/create" />
+      </Stack>
     </KeyboardProvider>
-  );
-}
-
-function SafeAreaSpecificToPlatform({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
-  if (Platform.OS === "ios") {
-    return <>{children}</>;
-  }
-  return (
-    <SafeAreaView style={{ flex: 1 }}>
-      {children}
-    </SafeAreaView>
   );
 }
