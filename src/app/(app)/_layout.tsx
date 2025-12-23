e3e8019d04b7cfdce6f335bb5e42c5951daca823
황교준 <hwangkyojun7806@gmail.com>
2025-12-23 19:16:40 +0900

    feat : 최신 변경사항 반영

--- src/app/(app)/_layout.tsx
+++ src/app/(app)/_layout.tsx
@@ -1,8 +1,24 @@
 import { Stack, useLocalSearchParams } from "expo-router";
 import { KeyboardProvider } from "react-native-keyboard-controller";
+import { Alert, NativeEventEmitter, NativeModules,Linking } from "react-native";
+import { useEffect } from "react";
+
+export function useVoiceDeepLink() {
+  useEffect(() => {
+    const sub = Linking.addEventListener("url", ({ url }) => {
+      if (url.includes("type=TORY_ALARM_ON")) {
+        Alert.alert("토리", "알람 켜 (Intent 딥링크)");
+      }
+    });
+
+    // 앱이 이미 떠있는 상태에서 open(url)로 들어오면 이걸로 잡힘
+    return () => sub.remove();
+  }, []);
+}
 
 export default function AppLayout() {
   const { setIsWebviewLoaded } = useLocalSearchParams();
+  useVoiceDeepLink();
 
   return (
     <KeyboardProvider statusBarTranslucent navigationBarTranslucent>
