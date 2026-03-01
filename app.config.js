dbb53835bd9464bf1c69cd908ac4549abb9268f2
Changmin Han <49056953+hcmhcs@users.noreply.github.com>
2026-03-01 17:35:28 +0900

    chore: v1.0.13 빌드번호2 testflight까지 배포

--- app.config.js
+++ app.config.js
@@ -10,7 +10,7 @@ export default {
     newArchEnabled: true,
     owner: "cheftory",
     ios: {
-      buildNumber: "1",
+      buildNumber: "2",
       entitlements: {
         "com.apple.security.application-groups": [
           "group.com.cheftory.cheftory",
